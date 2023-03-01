0510d72dcf32ec3de0881857965bfc1d72dbc672
ij-max <timijiks@gmail.com>
2023-03-01 21:58:43 +0100

    first commit

---
+++ max.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+
+})
+
+
+document.querySelectorAll(".scroll-down").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+}))
